bcc969b0242571a65537b5a85e96d6567d4cda58
Aurora <auroracichos7@gmail.com>
2024-03-06 22:27:51 -0700

    testing to see if fix vm

--- nixos/modules/system.nix
+++ nixos/modules/system.nix
@@ -14,7 +14,7 @@
   # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.11";
