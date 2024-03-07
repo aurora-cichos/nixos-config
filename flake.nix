84fe30223093763cd73c4cf0614451d70b98bdf5
Aurora <auroracichos7@gmail.com>
2024-03-07 04:55:59 -0700

    fixed error in flake.nix

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
     hyprland.url = "github:hyprwm/Hyprland";
 
     nixvim = {
-      url = "github:nix-community/nixvim-23.11";
+      url = "github:nix-community/nixvim/nixos-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
