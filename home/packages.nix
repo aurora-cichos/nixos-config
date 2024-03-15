472d98ef90c1afa85a6d46dff44fb93183e460e3
aurora-cichos <auroracichos7@gmail.com>
2024-03-14 21:45:44 -0600

    added packages

--- home/packages.nix
+++ home/packages.nix
@@ -1,10 +1,12 @@
 {pkgs, ...}: {
   nixpkgs.config.allowUnfree = true;
   home.packages = with pkgs; [
+    calcurse
     dconf
     libreoffice-fresh
     pulseaudio
     spotify
     steam
+    vencord
   ];
 }
