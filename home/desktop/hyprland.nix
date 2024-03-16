2c71357a0dd8b23c5dc1d9329e40990e499551d2
aurora-cichos <aurora-cichos7@gmail.com>
2024-03-16 14:44:38 -0600

    added to hyprland config

--- home/desktop/hyprland.nix
+++ home/desktop/hyprland.nix
@@ -6,14 +6,21 @@ let
 
     sleep 1
 
-    ${pkgs.swww}/bin/swww img ${../../theme/wallpaper.png} &
+    ${pkgs.swww}/bin/swww img ${/home/luna/nixos-config/theme/wallpaper.png} &
   '';
 in {
   wayland.windowManager.hyprland = {
     enable = true;
+
     settings = {
       # startup
       exec-once = ''${startupScript}/bin/start'';
+
+      # decor
+      decoration = {
+        shadow_offset = "0 5";
+        "col.shadow" = "rgba(00000099)";
+      };
 
       # binds
       "$mod" = "SUPER";
@@ -21,6 +28,13 @@ in {
       bind = [
         "$mod, RETURN, exec, kitty"
         "$mod, f, exec, firefox"
+        "$mod, SPACE, exec, rofi -show drun"
+        "$mod, MINUS, killactive"
+
+        "$mod, h, movefocus, l"
+        "$mod, l, movefocus, r"
+        "$mod, k, movefocus, u"
+        "$mod, j, movefocus, d"
       ];
 
       bindm = [
