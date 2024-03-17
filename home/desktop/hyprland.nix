3840100b906200d3c8d2d6878e4cf94aea45c84e
aurora-cichos <aurora-cichos7@gmail.com>
2024-03-16 18:38:07 -0600

    waybar

--- home/desktop/hyprland.nix
+++ home/desktop/hyprland.nix
@@ -11,6 +11,22 @@
 
     ${pkgs.swww}/bin/swww img ${/home/luna/nixos-config/theme/wallpaper.png} &
   '';
+
+  mod = "SUPER";
+
+  # binds ${mod} + [shift +] {1..10} to [move to] workspace {1..10} (stolen from fufie)
+  workspaces = builtins.concatLists (builtins.genList (
+      x: let
+        ws = let
+          c = (x + 1) / 10;
+        in
+          builtins.toString (x + 1 - (c * 10));
+      in [
+        "${mod}, ${ws}, workspace, ${toString (x + 1)}"
+        "${mod} SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+      ]
+    )
+    10);
 in {
   wayland.windowManager.hyprland = {
     enable = true;
@@ -51,23 +67,23 @@ in {
       ];
 
       # binds
-      "$mod" = "SUPER";
+      bind =
+        [
+          "${mod}, RETURN, exec, kitty"
+          "${mod}, f, exec, firefox"
+          "${mod}, SPACE, exec, rofi -show drun"
+          "${mod}, MINUS, killactive"
 
-      bind = [
-        "$mod, RETURN, exec, kitty"
-        "$mod, f, exec, firefox"
-        "$mod, SPACE, exec, rofi -show drun"
-        "$mod, MINUS, killactive"
-
-        "$mod, h, movefocus, l"
-        "$mod, l, movefocus, r"
-        "$mod, k, movefocus, u"
-        "$mod, j, movefocus, d"
-      ];
+          "${mod}, h, movefocus, l"
+          "${mod}, l, movefocus, r"
+          "${mod}, k, movefocus, u"
+          "${mod}, j, movefocus, d"
+        ]
+        ++ workspaces;
 
       bindm = [
-        "$mod, mouse:272, movewindow"
-        "$mod, mouse:273, resizewindow"
+        "${mod}, mouse:272, movewindow"
+        "${mod}, mouse:273, resizewindow"
       ];
     };
   };
--- home/desktop/waybar.nix
+++ home/desktop/waybar.nix
@@ -1,5 +1,121 @@
-{pkgs, ...}: {
+{pkgs, config, ... }: 
+let
+  css = with config.colorScheme.palette; ''
+    * {
+      border: none;
+      border-radius: 0;
+      font-family: JetBrainsMono Nerd Font;
+      font-size: 14px;
+      min-height: 0;
+    }
+
+    window#waybar {
+      background: transparent;
+      color: #${base05};
+    }
+
+    #workspaces {
+      background-color: #${base00};
+      margin: 5px;
+      margin-left: 10px
+      border-radius: 5px;
+    }
+    #workspaces button {
+      padding: 5px 10px;
+      color: #${base08};
+    }
+    #workspaces button.focused {
+      color: #${base02};
+      background-color: #${base05};
+      border-radius: 5px;
+    };
+    #workspaces button.hover {
+      background-color: #${base0C};
+      color: #${base08};
+    }
+
+    #clock, #pulseaudio, #network {
+      background-color: #${base08};
+      padding: 5px 10px;
+      margin 5px 0px;
+    }
+
+    #clock {
+      color: #${base0E};
+      border-radius: 0px 5px 5px 0px;
+      margin-right: 10px;
+    }
+
+    #network {
+      color: #${base0F};
+      border-radius: 5px 0px 0px 5px;
+    }
+
+    #pulseaudio {
+      color: #${base09};
+    }
+  '';
+in {
   programs.waybar = {
     enable = true;
+    style = css;
+
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "bottom";
+        height = 30;
+        spacing = 7;
+
+        modules-left = [
+          "hyprland/workspaces"
+        ];
+        modules-center = [];
+        modules-right = ["pulseaudio" "network" "clock"];
+
+        "hyprland/workspaces" = {
+          on-click = "activate";
+          format = "{icon}";
+          active-only = false;
+          format-icons = {
+            "1" = "一";
+            "2" = "二";
+            "3" = "三";
+            "4" = "四";
+            "5" = "五";
+            "6" = "六";
+            "7" = "七";
+            "8" = "八";
+            "9" = "九";
+            "10" = "十";
+          };
+
+          persistent_workspaces = {
+            "*" = 10;
+          };
+        };
+
+        pulseaudio = {
+          scroll-step = 5;
+          tooltip = true;
+          tooltip-format = "{volume}% {format_source}";
+          on-click = "${pkgs.killall}/bin/killall pavucontrol || ${pkgs.pavucontrol}/bin/pavucontrol";
+          format = " {icon}\n{volume}%";
+          format-bluetooth = "󰂯 {icon} {volume}%";
+          format-muted = "󰝟 ";
+          format-icons = {
+            default = ["" "" " "];
+          };
+        };
+        network = {
+          format-wifi = "󰤨";
+          format-ethernet = "󰤨";
+          format-alt = "󰤨"; format-disconnected = "󰤭"; tooltip-format = "{ipaddr}/{ifname} via {gwaddr} ({signalStrength}%)";
+        };
+        clock = {
+          format = "󰅐 {:%H:%M}";
+        };
+      };
+    };
   };
 }
