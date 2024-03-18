b82be58bb665c7edab0e9a93fac30bd5537118cb
aurora-cichos <aurora-cichos7@gmail.com>
2024-03-17 18:35:31 -0600

    git password management?

--- home/cli/git.nix
+++ home/cli/git.nix
@@ -3,5 +3,10 @@
     enable = true;
     userEmail = "aurora-cichos7@gmail.com";
     userName = "aurora-cichos";
+
+    extraConfig = {
+      credential.helper = "${
+        pkgs.git.override { withLibsecret = true; }
+      }/bin/git-credential-libsecret";
   };
 }
