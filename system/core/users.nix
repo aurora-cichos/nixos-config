b197c774af4899fe714c86955e90fd3f8ad078e2
aurora-cichos <aurora-cichos7@gmail.com>
2024-03-20 18:12:30 -0600

    groups

--- system/core/users.nix
+++ system/core/users.nix
@@ -2,7 +2,7 @@
   users.users.luna = {
     isNormalUser = true;
     description = "luna";
-    extraGroups = ["audio" "networkmanager" "video" "wheel"];
+    extraGroups = ["audio" "networkmanager" "video" "wheel" "input" "disk" "libvirtd"];
   };
 
   users.defaultUserShell = pkgs.zsh;
